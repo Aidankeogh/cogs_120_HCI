d2ce1eb6d612e016d2527e67aff91ed7508ad161
Aidan Keogh <aidankeogh@gmail.com>
2018-10-28 16:05:38 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import yaml
+
+
+# Check out flask cheat sheet here
+# https://s3.us-east-2.amazonaws.com/prettyprinted/flask_cheatsheet.pdf
+
+app = Flask(__name__)
+Bootstrap(app)
+
+with open('backend.yaml', 'r') as f:
+    backend = yaml.load(f)
+
+@app.route('/')
+def home():
+    return render_template('home.html', backend = backend)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', backend = backend)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', backend = backend)
+
+@app.route('/xyz')
+def event():
+    return render_template('event.html', backend = backend, event = backend['events']['xyz'])
+
+@app.route('/xyz/edit')
+def edit_event():
+    return render_template('home.html', backend = backend, event = backend['events']['xyz'])
+
+@app.route('/percy')
+@app.route('/jane_doe')
+def user():
+    return render_template('profile.html', backend = backend, event = backend['users']['jane_doe'])
+
+
+
+app.run(debug=True)
