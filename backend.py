617c221ee2e122dde37cd9c447809314376229b9
Aidan Keogh <aidankeogh@gmail.com>
2018-11-13 04:38:17 -0800

    Added a bunch of modals

--- backend.py
+++ backend.py
@@ -12,32 +12,24 @@ with open('templates/user_tile.html') as file:
 
 badges = backend['badges']
 
-def get_tiles(user):
+def get_tiles():
     for event in backend['events']:
         event = backend['events'][event]
-        event['hosting'] = (event['host'] == user)
+        event['hosting'] = (event['host'] == backend['user'])
         badge_counts = [0] * len(badges)
         attendees = 0
         friends = 0
 
         for attendee in event['attendees'] + [event['host']]:
             attendees += 1
-            if attendee in backend['users'][backend['user']]['friends']:
+            if backend['logged_in'] and attendee in backend['users'][backend['user']]['friends']:
                 friends += 1
             for i in range(len(badges)):
                 if backend['users'][attendee]['badges'][i]:
                     badge_counts[i] += 1
 
-        event['event_tile'] = event_template.render(current_user=user,
-                                                    event=event,
-                                                    host=backend['users'][event['host']],
-                                                    attendees=attendees,
-                                                    friends=friends,
-                                                    badges=badges,
-                                                    badge_counts=badge_counts)
+        event['friends'] = friends
+        event['num_attendees'] = attendees
+        event['badge_counts'] = badge_counts
 
-for user, user_profile in backend['users'].iteritems():
-    user_profile['user_tile'] = user_template.render(user=user_profile, badges=badges)
-    get_tiles(user)
-
-get_tiles(backend['user'])
+get_tiles()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 from backend import backend, get_tiles
 import yaml
@@ -11,28 +11,54 @@ Bootstrap(app)
 
 
 
-@app.route('/')
-def home():
-    return render_template('search.html', backend = backend, user=backend['user'])
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html', backend = backend)
-
-@app.route('/about')
-def about():
-    return render_template('about.html', backend = backend)
-
-@app.route('/search')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+    if request.method == 'POST': #this block is only entered when the form is submitted
+        r = request.form
+        print r
+        if  'edit' in r: # attend event
+            link = r['edit']
+            e = backend['events'][link]
+            e['title'] = r[link+'title']
+            e['date'] = r[link+'date']
+            e['location'] = r[link+'location']
+            e['description'] = r[link+'description']
+        if  'attend' in r: # attend event
+            link = r['attend']
+            e = backend['events'][link]
+            e['attendees'].append(r[link+'name'])
+        if  'unattend' in r: # attend event
+            link = r['unattend']
+            e = backend['events'][link]
+            e['attendees'].remove(r[link+'name'])
+        return redirect(request.path,code=302)
+
     return render_template('search.html', backend = backend)
 
-@app.route('/new_event')
-def new_event():
-    return render_template('new_event.html', backend = backend)
-
-@app.route('/<something>')
+@app.route('/<something>', methods=['GET', 'POST'])
 def other_profile(something):
+
+    if request.method == 'POST': #this block is only entered when the form is submitted
+        r = request.form
+        print r
+        if  'edit' in r: # attend event
+            link = r['edit']
+            e = backend['events'][link]
+            e['title'] = r[link+'title']
+            e['date'] = r[link+'date']
+            e['location'] = r[link+'location']
+            e['description'] = r[link+'description']
+        if  'attend' in r: # attend event
+            link = r['attend']
+            e = backend['events'][link]
+            e['attendees'].append(r[link+'name'])
+        if  'unattend' in r: # attend event
+            link = r['unattend']
+            e = backend['events'][link]
+            e['attendees'].remove(r[link+'name'])
+        return redirect(request.path,code=302)
+
     if something in backend['users']:
         user = something
         return render_template('profile.html', backend = backend, user=user)
@@ -41,6 +67,33 @@ def other_profile(something):
         return render_template('event.html', backend = backend, event=backend['events'][event])
     else:
         return render_template('404')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST': #this block is only entered when the form is submitted
+        backend['logged_in'] = True
+
+        if request.form['username'] not in backend['users']:
+            backend['user'] = 'percy'
+        else:
+            backend['user'] = request.form['username']
+
+    return render_template('login.html', backend = backend)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', backend = backend)
+
+@app.route('/favicon.ico')
+def favicon():
+    return 'null'
+
+@app.route('/new_event', methods=['GET', 'POST'])
+def new_event():
+    return render_template('new_event.html', backend = backend)
+
+
 
 
 
